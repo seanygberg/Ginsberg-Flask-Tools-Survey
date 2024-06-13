841d4e94a6ef65c223905dfe8be5192d1028c1a9
seanygberg <60160094+seanygberg@users.noreply.github.com>
2024-06-13 07:20:22 -0400

    first commit

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.secret_key = "do-not-do-this"
 
-responses = []
-
 @app.route('/')
 def survey_init():
     return render_template("index.html", survey=satisfaction_survey)
 
+@app.route("/start", methods=["POST"])
+def start_survey():
+    session["responses"] = []
+    return redirect("/questions/0")
+
+
 @app.route('/questions/<int:qid>')
 def show_question(qid):
-    questions = satisfaction_survey.questions
-    if qid >= len(questions):
+    responses = session.get("responses", [])
+    if qid >= len(satisfaction_survey.questions):
         flash("Invalid question access. Redirected to the start page.")
-        return redirect("/")
+        return redirect("/complete")
     if qid != len(responses):
         flash(f"Invalid question access. Redirected to question {len(responses)}")
         return redirect(f"/questions/{len(responses)}")
@@ -25,7 +29,9 @@ def show_question(qid):
 @app.route('/answer', methods=['POST'])
 def handle_response():
     answer = request.form['answer']
+    responses = session.get("responses", [])
     responses.append(answer)
+    session["responses"] = responses
 
     if len(responses) < len(satisfaction_survey.questions):
         return redirect(f"/questions/{len(responses)}")
